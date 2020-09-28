d55ea36fd877316f5dbd783b6d92fb01f30e94e7
sadaking <sichirigahama.37@gmail.com>
2020-09-28 18:10:03 +0900

    カレンダーの表示を修正

--- calendar.rb
+++ calendar.rb
@@ -1,34 +1,36 @@
 #<まずはじめに考慮すべきこと>
 #コードは簡潔にわかりやすく
-#閏年の計算が必要
-#ツェラー公式を用いるか否か
+#閏年の計算が必要　=>Date.newを用いることで必要なし
+#ツェラー公式を用いるか否か　=>Date.newを用いることで必要なし
+#その月の求め方=> ex)puts calendar(Date.new(2020,9))
 
 #日付を扱うことができるクラスを読み込む
 require 'date'
 
-  def calendar(select_date) #操作した日の日付
-    # 入力された月の初日を取得する
+   #"select_date"は操作した日の年月(ex.2020,9)
+  def calendar(select_date)
+    # 入力された月の初日の曜日数値を取得する
     first_day = Date.new(select_date.year,select_date.month,1)
     # 入力された月の第一日曜日を取得
     first_sunday = first_day - first_day.strftime('%w').to_i
 
     # 曜日の出力
-    week = %w(Su Mo Tu We Th Fr Sa)
-    puts week.join("|  ")
+     puts " Su|Mo|Tu|We|Th|Fr|Sa"
 
-    #繰り返し構文で回したい↓
+    #"select_date"で入力された月の日付を繰り返しで表示
     while first_sunday.month <= first_day.month
-    
+
       if first_sunday.month != first_day.month
-        print "    "
-      #土曜日のタイミングで改行をさせる↓↓
-    elsif first_sunday.strftime('%u') == "6"
-        print "   " + first_sunday.strftime('%e') + "\n"
-      #他の日は↓↓
+        print "   "
+      #土曜日が過ぎたタイミングで改行をさせる
+      elsif first_sunday.strftime('%u') == "6"
+        print " " + first_sunday.strftime('%e') + "\n"
+      #他の日
       else
-        print "   " + first_sunday.strftime('%e')
+        print " " + first_sunday.strftime('%e')
       end
 
+      #月末日まで繰り返す
       first_sunday += 1
     end
 
